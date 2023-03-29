7e6a8259e2ee8b6a93a89769cd89d656e1976bb7
neerajbafila@gmail.com <neerajbafila@gmail.com>
2023-03-29 16:52:28 +0530

    app.py updated

--- app.py
+++ app.py
@@ -111,7 +111,7 @@ def index():
 
 port = int(os.getenv("PORT"))
 if __name__ == "__main__":
-    #app.run(host='127.0.0.1', port=8001, debug=True)
+    # app.run(host='127.0.0.1', port=8001, debug=True)
 	# app.run(debug=True)
     app.run(host='0.0.0.0', port=port)
 
